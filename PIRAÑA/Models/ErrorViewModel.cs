1a46d22832ef82794c4c3b43ced42e1d6ffd613c
FRANK MARCO <FLOREZ_FRANK@USMP.PE>
2021-04-27 18:55:23 -0500

    a

---
+++ PIRAÑA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PIRAÑA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
